e9e2f5c83fb144058b205ac318c02a1d0e37ac46
Tiranych <krimyan2015@yandex.ru>
2024-07-12 13:16:49 +0400

    final commit

---
+++ src/components/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+/* import Header from "./header/Header.jsx"
+import Main from "./main/Main.jsx"
+import Footer from "./footer/Footer.jsx" */
+/* import * as styles from "../../styles/global.css"; */
+import "../styles/reset.css";
+
+const App = () => {
+    return (
+        <>
+            {/* <Header props={styles}/>
+            <Main props={styles}/>
+            <Footer props={styles}/> */}
+            руддщ
+        </>
+    )
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.jsx',
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: [
+                    { loader: 'style-loader' },
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            modules: true,
+                        },
+                    },
+                ],
+            },
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: ["babel-loader",],
+            },
+            {
+                test: /\.(png|jpg|gif)$/i,
+                use: [
+                    {
+                        loader: "url-loader",
+                        options: {
+                            limit: 8192,
+                            encoding: true,
+                        },
+                    },
+                ],
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, './public/index.html'),
+        }),
+    ],
+};

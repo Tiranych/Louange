e33f44355236920d52b0ca94c8e0026c3933d25e
Tiranych <krimyan2015@yandex.ru>
2024-08-08 12:15:16 +0400

    Final commit

---
+++ src/components/global/Modal.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import './Modal.css';
+
+const Modal = ({ active, setActive }) => {
+    const [formFilled, setFormFilled] = useState(false);
+
+    return (
+        <div className={active ? "modal_window modal_window__active" : "modal_window"} onClick={() => { setActive(false) }}>
+            <div className="modal__content" onClick={(e) => { e.stopPropagation() }}>
+                <button className="modal__close-btn" onClick={() => { setActive(false) }}>
+                    <img src="/images/global/modal/Close.svg" alt="" />
+                </button>
+                {!formFilled &&
+                    <>
+                        <p className="modal__title">Обсудить проект</p>
+                        <p className="modal__text">Заполните короткую форму обратной связи, и наши менеджеры свяжутся с вами в ближайшее время</p>
+                        <form className="modal__form">
+                            <input className="modal__input" type="text" placeholder="Ваше имя" required />
+                            <input className="modal__input" type="email" placeholder="Ваш e-mail" required />
+                            <textarea className="modal__input modal__textarea" placeholder="Опишите ваш проект" required></textarea>
+                            <div className="modal__row">
+                                <input type="checkbox" className="modal__checkbox" id="checkbox" required />
+                                <label htmlFor="checkbox"></label>
+                                <p className="modal__info">
+                                    Я согласен с <Link className="modal__link" to={`privacy-policy`}>политикой обработки персональных данных</Link> и
+                                    даю <Link className="modal__link" to={`privacy-policy`}>согласие</Link> на обработку своих персональных данных
+                                </p>
+                            </div>
+                            <button onClick={() => { setFormFilled(true) }} className="modal__submit-btn" type="submit">Отправить заявку</button>
+                        </form>
+                    </>
+                }
+                {formFilled &&
+                    <>
+                        <p className="modal__title">Ваша заявка отправлена</p>
+                        <p className="modal__text">Спасибо за вашу заявку! Ожидайте — наши менеджеры свяжутся с вами в самое ближайшее время!</p>
+                        <div className="modal__img">
+                            <picture >
+                                <source srcSet="/images/global/modal/DoneX64.svg" media="(max-width: 700px)" />
+                                <img src="/images/global/modal/DoneX96.svg" alt="" />
+                            </picture>
+                        </div>
+                    </>
+                }
+            </div>
+        </div>
+    )
+}
+
+export default Modal;
--- src/components/header/slogan/Slogan.jsx
+++ src/components/header/slogan/Slogan.jsx
@@ -1,12 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import * as styles from './Slogan.module.css';
+import Modal from "../../global/Modal.jsx";
 
 const Slogan = () => {
+
+    const [modalActive, setModalActive] = useState(false);
+
     return (
         <div className={styles.slogan}>
             <p className={styles.title}>Louange</p>
             <p className={styles.text}>Мечта об уютном пространстве — это реальность</p>
-            <button className={styles.btn}>Оставить заявку</button>
+            <button onClick={() => { setModalActive(true) }} className={styles.btn}>Оставить заявку</button>
+            <Modal active={modalActive} setActive={setModalActive} />
         </div>
     )
 }

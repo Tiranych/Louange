ec30ef239f3b0b2a39ce68c7211b9dfcb35237a1
Tiranych <krimyan2015@yandex.ru>
2024-08-08 17:00:11 +0400

    Added attribute "required" at all inputs, textareas and checkboxes at forms.

--- src/components/global/ContactUs.jsx
+++ src/components/global/ContactUs.jsx
@@ -14,11 +14,11 @@ const ContactUs = () => {
                 <div className={styles.box}>
                     <p className={styles.callToAct}>Сделайте ваш дизайн уникальным вместе с Louange</p>
                     <form className={styles.form}>
-                        <input className={styles.input} type="text" placeholder='Ваше имя' />
-                        <input className={styles.input} type="text" placeholder='Ваш e-mail' />
-                        <textarea className={styles.input + ' ' + styles.textarea} placeholder='Опишите ваш проект'></textarea>
+                        <input className={styles.input} required type="text" placeholder='Ваше имя' />
+                        <input className={styles.input} required type="text" placeholder='Ваш e-mail' />
+                        <textarea className={styles.input + ' ' + styles.textarea} required placeholder='Опишите ваш проект'></textarea>
                         <div className={styles.row}>
-                            <input className={styles.checkbox} type="checkbox" />
+                            <input className={styles.checkbox} required type="checkbox" />
                             <p className={styles.text}>Я согласен с <Link to={`/privacy-policy`} className={styles.link}>политикой обработки персональных данных</Link> и даю
                                 <Link to={`/privacy-policy`} className={styles.link}> согласие</Link> на обработку своих персональных данных
                             </p>
--- src/components/global/Form.jsx
+++ src/components/global/Form.jsx
@@ -7,11 +7,11 @@ const Form = () => {
         <form className={styles.form}>
             <div className={styles.inner}>
                 <p className={styles.title}>Остались вопросы? Напишите нам!</p>
-                <input className={styles.input} type="text" placeholder='Ваше имя' />
-                <input className={styles.input} type="text" placeholder='Ваш e-mail' />
-                <textarea className={styles.input + ' ' + styles.textarea} placeholder='Текст обращения'></textarea>
+                <input className={styles.input} required type="text" placeholder='Ваше имя' />
+                <input className={styles.input} required type="text" placeholder='Ваш e-mail' />
+                <textarea className={styles.input + ' ' + styles.textarea} required placeholder='Текст обращения'></textarea>
                 <div className={styles.row}>
-                    <input className={styles.checkbox} type="checkbox" />
+                    <input className={styles.checkbox} required type="checkbox" />
                     <p className={styles.text}>Я согласен с <Link to={`/privacy-policy`} className={styles.link}>политикой обработки персональных данных</Link> и даю 
                     <Link to={`/privacy-policy`} className={styles.link}> согласие</Link> на обработку своих персональных данных
                     </p>

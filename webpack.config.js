1a0f1c2f17c0a6f99f09f6a5c570c9623b820c08
Tiranych <krimyan2015@yandex.ru>
2024-08-21 10:04:39 +0300

    Final commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
     entry: './src/index.jsx',
@@ -12,6 +13,11 @@ module.exports = {
         new HtmlWebpackPlugin({
             template: './src/index.html',
         }),
+        new CopyPlugin({
+            patterns: [
+                { from: "public/images", to: "public/images" },
+            ],
+        })
     ],
     module: {
         rules: [
